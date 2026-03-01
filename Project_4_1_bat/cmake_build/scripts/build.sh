66c683af55a490e62235271ec2bba27890f43532
Zazz7res <harrylee_x@outlook.com>
2026-03-02 03:22:46 +0800

    git add

--- Project_4_1_bat/cmake_build/scripts/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-cd "$(dirname "$0")/.."
-rm -rf build && mkdir build && cd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE=../toolchain-arm-none-eabi.cmake -GNinja
-ninja
-echo -e "\n✅ 构建成功！输出文件："
-ls -lh *.elf *.bin *.hex *.map 2>/dev/null | grep -E '\.(elf|bin|hex|map)$'
